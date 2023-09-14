015f3cf17da6795a9e0a61f655f020d803cf1b5c
Younggyu <yonggyu1234@naver.com>
2023-09-14 12:01:02 +0900

    구구단

---
+++ 20230914/lecture10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for(int i = 2; i <= 9; i++) {
+        if(i == 5) continue;
+        for (int j = 1; j <= 9; j++) {
+            printf("%d x %d = %d\n", i, j, i*j);
+        }
+    }
+
+    return 0;
+}
