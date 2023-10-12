b3ed3c6f695be8e0bfa4e0e43ace703664ccebf8
yonggyu <yonggyu1234@naver.com>
2023-10-12 10:18:23 +0900

    포인터 연산자 *

---
+++ 20231012/lecture2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 100, num2 = 100;
+    int* pnum;
+
+    pnum = &num1;
+    (*pnum) += 30;
+
+    pnum = &num2;
+    (*pnum) -= 30;
+
+    printf("num1 : %d\n", num1);
+    printf("num2 : %d\n", num2);
+
+    return 0;
+}
